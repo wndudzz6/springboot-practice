b0508ad4d1949027fe871cf491dc05376caebf10
O ju yeong <osmzxcvb@naver.com>
2024-07-10 15:40:26 +0900

    7/10 블로그 개발 전 연습

---
+++ src/main/java/wndud/o/springbootdeveloper/MemberService.java
@@ -0,0 +1,27 @@
+package wndud.o.springbootdeveloper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    public void test() {
+        // 생성(create)
+        memberRepository.save(new Member(1L, "A"));
+
+        // 조회
+        Optional<Member> member = memberRepository.findById(1L); // 단건 조회
+        List<Member> allMembers = memberRepository.findAll(); // 전체 조회
+
+        // 삭제
+        memberRepository.deleteById(1L);
+    }
+}
+
